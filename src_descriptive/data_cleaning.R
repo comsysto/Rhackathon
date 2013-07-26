c69335f4ce8e7552b3b3f0fc38ee289b416dc724
Dr. rer. nat. Markus Schmidberger <markus.schmidberger@googlemail.com>
2013-07-26 18:29:33 +0200

    using new input file

--- src_descriptive/data_cleaning.R
+++ src_descriptive/data_cleaning.R
@@ -1,5 +1,5 @@
 # read data
-data <- read.csv("data/techism-eventdata-20130725.csv", 
+data <- read.csv("data/techism-events-local-tz.csv", 
                  sep=",", fileEncoding="UTF-8", stringsAsFactors=FALSE)
 
 # set empty date_time_end to NA
@@ -7,7 +7,7 @@ emptyid <- which( data[, "date_time_end"]=="" )
 data[emptyid, "date_time_end"] <- NA
 
 # transform date
-format <- "%Y-%m-%dT%H:%M:%S+00:00"
+format <- "%Y-%m-%dT%H:%M:%S"
 tmp <- as.POSIXlt( data[, "date_time_begin"], format=format)
 tmp2 <- as.POSIXlt( data[, "date_time_end"], format=format)
 data <- cbind(data,date_time_begin_new=tmp,date_time_end_new=tmp2)
