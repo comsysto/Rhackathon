25aa56fdae0bcd74e604f313259137dae4241ccf
Dr. rer. nat. Markus Schmidberger <markus.schmidberger@googlemail.com>
2013-07-29 13:07:17 +0200

    small bug fix for time converting

--- src_descriptive/descriptive_analyses.R
+++ src_descriptive/descriptive_analyses.R
@@ -3,11 +3,12 @@ load("data//data_clean.Rdata")
 # which time the most meetup
 time_start <- format(data[, "date_time_begin_new"], "%X")
 sort( table(time_start) )
-time_end <- format(data[, "date_time_begin_new"], "%X")
+time_end <- format(data[, "date_time_end_new"], "%X")
 sort( table(time_end) )
 
 # how long is a meetup
 runtime <- as.numeric( data[, "date_time_end_new"] - data[, "date_time_begin_new"] )/60
+sort(table(runtime))
 summary(runtime)
 
 # best weekday
